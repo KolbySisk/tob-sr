c371152b96edb80ec932918d9918b93ddddf7f99
Kolby <kolby.sisk@gmail.com>
2022-03-01 02:40:34 -0600

    add slack commands

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 'use strict';
 
+import { createEventAdapter } from '@slack/events-api';
+import localtunnel from 'localtunnel';
 import inquirer from 'inquirer';
 import { State } from './state';
 import { getNumberFromRegion, getRegion, initControls } from './utils';
 import * as banking from './banking';
 import * as mining from './mining';
 import * as thievingKnight from './thieving-knight';
+
+import 'dotenv/config';
 
 export const state = new State({ paused: false });
 
@@ -40,4 +44,35 @@ const init = async () => {
   scripts[script as keyof typeof scripts].init();
 };
 
-init();
+const initSlack = async () => {
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+  const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
+  if (!slackSigningSecret) {
+    throw new Error('Add SLACK_SIGNING_SECRET to .env file');
+  }
+
+  // Start a tunnel using https://github.com/localtunnel/localtunnel
+  await localtunnel({ port, subdomain: 'tob-sr' });
+
+  const slackEvents = createEventAdapter(slackSigningSecret);
+
+  slackEvents.on('message', (event: { text: string }) => {
+    if (event.text === 'quit') {
+      process.exit();
+    } else if (event.text === 'pause') {
+      state.setPaused(!state.paused);
+    }
+  });
+
+  // Start the built-in server
+  const server = await slackEvents.start(port);
+
+  // Log a message when the server is ready
+  console.log(`Listening for events on ${server?.address()}`);
+};
+
+(async () => {
+  await initSlack();
+  init();
+})();
